b2ff5818e0e75cf7ea9bef9303d881713d5a72e4
Oleg Atamankin <aov013@mail.ru>
2019-05-12 16:07:24 +0300

    add var class Sputnik

--- src/main/java/sputnik/Sputnik.java
+++ src/main/java/sputnik/Sputnik.java
@@ -1,5 +1,23 @@
 package sputnik;
 
 public class Sputnik {
+    private String name;
+    private String designator;
+    private int apogee;
+    private int perigee;
+    private int inclination;
+    private double altitude;
+    private double velocity;
+    private double period;
 
+    public Sputnik(String name, String designator, int apogee, int perigee, int inclination, double altitude, double velocity, double period) {
+        this.name = name;
+        this.designator = designator;
+        this.apogee = apogee;
+        this.perigee = perigee;
+        this.inclination = inclination;
+        this.altitude = altitude;
+        this.velocity = velocity;
+        this.period = period;
+    }
 }
